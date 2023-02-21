ca532a6eed0517d700f00565ada4812818f0ecc6
Petr <stepanchencko88@gmail.com>
2023-02-22 01:01:21 +0200

    display_employees - function completed

--- DZ/DZ9.py
+++ DZ/DZ9.py
@@ -2,7 +2,8 @@ import csv
 
 
 def new_file(list_users):
-    with open("new_employees.csv", "w", newline="") as file:
+    name = input("Enter a name for the new file with the CSV extension.")
+    with open(name, "w", newline="") as file:
         columns = ["name", "surname", "age", "job title"]
         writer = csv.DictWriter(file, fieldnames=columns)
         writer.writeheader()
@@ -43,6 +44,29 @@ def print_open_file(name_of_file):
             )
             count += 1
         print('+', '-' * 3, '+', '-' * 15, '+', '-' * 25, '+', '-' * 3, '+', '-' * 11, '+', sep="")
+
+
+def print_list(some_list):
+    count = 1
+    print('+', '-' * 3, '+', '-' * 15, '+', '-' * 25, '+', '-' * 3, '+', '-' * 11, '+', sep="")
+    print(
+        f'|', "Num".center(3),
+        f'|', "Name".center(15),
+        f'|', "Surname".center(25),
+        f'|', "Age".center(3),
+        f'|', "Job title".center(11), "|", sep=""
+    )
+    print('+', '-' * 3, '+', '-' * 15, '+', '-' * 25, '+', '-' * 3, '+', '-' * 11, '+', sep="")
+    for employee in some_list:
+        print(
+            f'|', str(count).center(3),
+            f'|', employee["name"].center(15),
+            f'|', employee["surname"].center(25),
+            f'|', str(employee["age"]).center(3),
+            f'|', employee["job title"].center(11), "|", sep=""
+        )
+        count += 1
+    print('+', '-' * 3, '+', '-' * 15, '+', '-' * 25, '+', '-' * 3, '+', '-' * 11, '+', sep="")
 
 
 def add_employee(name_of_file):
@@ -121,8 +145,59 @@ def search_employee():
     pass
 
 
-def display_employees():
-    pass
+def display_employees(name_of_file):
+    action = int(input(f"Please choose one of the options below:\n"
+                       f"Press <1> to display information about all employees.\n"
+                       f"Press <2> to display information about employees of the specified age.\n"
+                       f"Press <3> to display information about employees whose last name begins "
+                       f"with the specified letter\n"
+                       f" - > \n"))
+    if action in range(1, 4) and action == 1:
+        print_open_file(name_of_file)
+
+    elif action in range(1, 4) and action == 2:
+        need_age = input("Please, enter the age for the search ->")
+        result_list = []
+        with open(name_of_file, "r", newline="") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["age"] == need_age:
+                    result_list.append(row)
+        if len(result_list) == 0:
+            print(f"No employees under the age of {need_age} years were found.")
+        elif len(result_list) > 0:
+            print(f"{len(result_list)} employees in age {need_age} years were found.")
+            print_list(result_list)
+            act = input("Would you like to save your search results? \n"
+                        "Press <Y>  to save - any value to cancel ->")
+            if act.upper() == "Y":
+                new_file(result_list)
+
+    elif action in range(1, 4) and action == 3:
+        need_letters = input("Please, enter first letters for the search ->")
+        result_list = []
+        with open(name_of_file, "r", newline="") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if (row["surname"][0:len(need_letters)]).upper() == need_letters.upper():
+                    result_list.append(row)
+        if len(result_list) == 0:
+            print("Nothing found.")
+        elif len(result_list) > 0:
+            print(f"Found {len(result_list)} employees with entered letters ({need_letters}) in the surname.")
+            print_list(result_list)
+            act = input("Would you like to save your search results? \n"
+                        "Press <Y>  to save - any value to cancel ->")
+            if act.upper() == "Y":
+                new_file(result_list)
+
+
+
+
+
+
+
+
 
 
 edit_list = [
@@ -134,5 +209,7 @@ edit_list = [
 FILENAME = "new_employees.csv"
 # edit_employee(FILENAME)
 # print_open_file(FILENAME)
-del_employee(FILENAME)
+# del_employee(FILENAME)
+
+display_employees(FILENAME)
 

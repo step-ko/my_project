31ef71c0b49e5abcadf4548fd7d6f88f8d02b39f
Petr <stepanchencko88@gmail.com>
2023-02-21 02:11:36 +0200

    Some changes ... WIP

--- DZ2.py
+++ DZ2.py
@@ -5,29 +5,59 @@
 """
 import random
 
-min_value, max_value, count = 0, 100, 0
+# min_value, max_value, count = 0, 100, 0
+#
+#
+# def input_number() -> int:
+#     while True:
+#         try:
+#             number = int(input("Name any number from 1 to 100 -> "))
+#             if 0 < number < 101:
+#                 break
+#         except ValueError:
+#             print("Error. Try again.")
+#     return number
+#
+#
+# print("Welcome to the game - Guess the Number !")
+# secret_number = input_number()
+# while True:
+#     count += 1
+#     guessed_number = (min_value + max_value) // 2
+#     if secret_number == guessed_number:
+#         print(f"Your number is {guessed_number}, i was able to guess it in {count} tries")
+#         break
+#     elif secret_number < guessed_number:
+#         max_value = guessed_number - 1
+#     else:
+#         min_value = guessed_number + 1
 
 
-def input_number() -> int:
+def input_guess() -> int:
     while True:
         try:
-            number = int(input("Name any number from 1 to 100 -> "))
-            if 0 < number < 101:
+            number = int(input("Enter any number from 1 to 100 -> "))
+            if 0 <= number <= 100:
                 break
         except ValueError:
             print("Error. Try again.")
     return number
 
 
-print("Welcome to the game - Guess the Number !")
-secret_number = input_number()
+print("Welcome to the game - Guess the Number!")
+print("I will choose a number from 1 to 100 and you have to guess it.")
+secret_number = random.randint(0, 101)
+
+guesses, count = [], 0
+
 while True:
     count += 1
-    guessed_number = (min_value + max_value) // 2
-    if secret_number == guessed_number:
-        print(f"Your number is {guessed_number}, i was able to guess it in {count} tries")
-        break
-    elif secret_number < guessed_number:
-        max_value = guessed_number - 1
+    guess = input_guess()
+    if guess in guesses:
+        print("You have already entered this number, try another one.")
     else:
-        min_value = guessed_number + 1
+        if guess == secret_number:
+            print(f"Congratulations! You guessed the number {secret_number} in {count} tries")
+            break
+        else hint = less more
+            print(f"Sorry, but you're wrong. My number is {hint}.Try again.")
